f52624255d24811ea454d4e665d9d6fc78c9079f
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    beryl: Inherit launch_with_vendor_ramdisk for vAB
     * MT6855 devices have a vendor_boot partition.
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
 # Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
     checkpoint_gc \
