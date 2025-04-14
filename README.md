642c5ce79e52fd2facceb7582804b3b85eaf44a2
atharvnegi <atharvnegi2951@gmail.com>
2025-04-14 20:54:28 +0530

    beryl: Initial Skeleton Trees
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Redmi Note 14 5G/Poco M6 Pro 5G (beryl)
+
+```
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_beryl.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from beryl device
+$(call inherit-product, device/xiaomi/beryl/device.mk)
+
+PRODUCT_DEVICE := beryl
+PRODUCT_NAME := lineage_beryl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 14 5G
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryl-user 15 AP3A.240905.015.A2 OS2.0.3.0.VOQINXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/beryl/beryl:12/SP1A.210812.016/OS2.0.3.0.VOQINXM:user/release-keys
