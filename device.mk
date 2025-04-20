c7921322902360e9dfdc6fce669ad934670ee71d
atharvnegi <atharvnegi2951@gmail.com>
2025-04-20 07:29:19 +0530

    beryl: Switch to stock usb service
    Change-Id: I1d542ac644dfd50fd56231bdac0e0e82f4856d05
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -346,8 +346,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.mediatek
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
 
 # VNDK
 PRODUCT_PACKAGES += \
