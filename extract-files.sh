2900b4ad2c9df93b98e051e892a12e11d64191fa
atharvnegi <atharvnegi2951@gmail.com>
2025-05-12 17:29:13 +0530

    beryl: Link keymint service to v3
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,10 @@ function blob_fixup {
             "$PATCHELF" --add-needed "libaudioclient_shim.so" "$2"
             [ "$2" = "" ] && return 0
             ;;
+        vendor/bin/hw/android.hardware.security.keymint@1.0-service.mitee)
+            "$PATCHELF" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V3-ndk.so" "$2"
+            [ "$2" = "" ] && return 0
+            ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
             [ "$2" = "" ] && return 0
