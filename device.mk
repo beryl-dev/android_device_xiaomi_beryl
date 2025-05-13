a9d4a664a81f0ba57969753bb8956a84ad04a0bf
SGCMarkus <markusornik@gmail.com>
2025-05-13 05:33:20 +0530

    beryl: Add security.rkp-V3 dependency to keymint service
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -218,6 +218,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint-V3-ndk.vendor \
+    android.hardware.security.rkp-V3-ndk.vendor \
     android.hardware.security.secureclock-V1-ndk_platform.vendor \
     android.hardware.security.sharedsecret-V1-ndk_platform.vendor
 
